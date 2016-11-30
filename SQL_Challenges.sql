d1d2af72174224d79f700b71ddd03f1942abd371
Matthew-Mitchell <matthewbmitchell@gmail.com>
2016-11-30 09:49:22 -0500

    Update SQL_Challenges.sql

--- SQL_Challenges.sql
+++ SQL_Challenges.sql
@@ -35,32 +35,37 @@ SELECT COUNT(DISTINCT(person_id)) from url_final_new WHERE person_id IN (SELECT 
 SELECT Search_date FROM search_final WHERE person_id IN (SELECT person_de
 d FROM person_demos_final JOIN url_final_new ON person_demos_final.person_id=url_fin WHERE domain_name='vw.com' ORDER BY RANDOM() LIMIT 100) ORDER BY Search_date LIMIT 1;
 
+\\UPDATED:
+SELECT url_final_new.person_id, MIN(url_final_new.calendar_day) INTO #TempVW FROM url_final_new JOIN person_demos_final USING(person_id) WHERE person_demos_final.person_id IN(SELECT distinct(person_demos_final.person_id)sele FROM person_demos_final JOIN url_final_new USING(person_id) WHERE url_final_new.domain_name='vw.com' ORDER BY RANDOM() LIMIT 100) GROUP BY url_final_new.person_id;
 
 \\For the 100 random vw users, what is the visit rate to twitter.com?
 SELECT COUNT(*) FROM url_final_new WHERE person_id IN (SELECT person_demos_final.person_id FROM person_demos_final JOIN url_final_new USING(person_id) WHERE url_final_new.domain_name='vw.com' ORDER BY RANDOM() LIMIT 100) AND url_final_new.domain_name='twitter.com';
 
 \\Select a random sample of 100 users that did not visit vw.com.
-SELECT person_id FROM person_demos_final JOIN url_final_new USING(person_id) WHERE NOT url_final_new.domain_name='vw.com' ORDER BY RANDOM() LIMIT 100;
+SELECT person_id into #NotVW FROM person_demos_final JOIN url_final_new USING(person_id) WHERE NOT url_final_new.domain_name='vw.com' ORDER BY RANDOM() LIMIT 100;
 
 \\What is the index of visit rates to twitter.com for the vw versus non vw users?
 SELECT COUNT(*) FROM url_final_new WHERE person_id IN (SELECT person_demos_final.person_id FROM person_demos_final JOIN url_final_new USING(person_id) WHERE NOT url_final_new.domain_name='vw.com' ORDER BY RANDOM() LIMIT 100) AND url_final_new.domain_name='twitter.com';
 
-\\What is the top site visited prior to the users hitting vw.com?
-\\Tmp table; person_id, 1st visit to vw.com
+\\UPDATED:
+select count(*) from url_final_new join #NotVW using(person_id) where url_final_new.domain_name='twitter.com';
+select count(*) from url_final_new join #TempVW using(person_id) where url_final_new.domain_name='twitter.com';
 
+\\What is the top site visited prior to the users hitting vw.com?
+\\Tmp table; person_id, 1st visit to vw.com (updated and referenced previously)
 SELECT url_final_new.person_id, MIN(url_final_new.calendar_day) INTO #TempVW FROM url_final_new JOIN person_demos_final USING(person_id) WHERE person_demos_final.person_id IN(SELECT distinct(person_demos_final.person_id)sele FROM person_demos_final JOIN url_final_new USING(person_id) WHERE url_final_new.domain_name='vw.com' ORDER BY RANDOM() LIMIT 100) GROUP BY url_final_new.person_id;
 
 \\Join this temp table to url_final_new and filter based on when calendar_day<vw visit
-
+\\First attempt; incorrect solution
 SELECT url_final_new.domain_name, count(*) FROM url_final_new JOIN #TempVW USING(person_id) WHERE url_final_new.person_id IN (#TempVW.person_id) AND url_final_new.calendar_day< #TempVW.min GROUP BY url_final_new.domain_name ORDER BY COUNT(url_final_new.domain_name) DESC;
 
-
+\\Updated correct solution using diffdate(time, start, end)
 select url_fina_new.domain_name, count(*) from url)final)new join #TempVW using(person_id) wher diffdate(day, url_final_new.calendar_day, #TempVW) < 0 group by url_final_new.domain_name order b count(*) desc;
 
 
 
 \\What are the defining demographics of the random 100 vw.com visitors?
-
+\\Could be improved
 SELECT * FROM person_demos_final AS p JOIN #TempVW USING(person_id) GROUPBY p.gender, p.age, p.ethnicity;
 
 \\Dataset is huge; generating random samples and testing queries on this limited domain is typically adventageous;
